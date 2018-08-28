ed7eaa724478f527afa507906ae472e158077746
kuanglinfei <helloklf@outlook.com>
2018-08-28 17:31:02 +0800

    更新

---
+++ app/src/main/assets/custom/switchs/force_resize_get.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+
+if [[ `settings get global force_resizable_activities` = '1' ]]
+then
+    echo 1
+else
+    echo 0
+fi
---
+++ app/src/main/assets/custom/switchs/force_resize_set.sh
@@ -0,0 +1,4 @@
+#!/system/bin/sh
+state=$1
+
+settings put global force_resizable_activities $state
