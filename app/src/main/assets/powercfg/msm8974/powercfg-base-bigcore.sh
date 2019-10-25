3489dd58cd4c58973becc4b403d1e71a394a684c
kuanglinfei <helloklf@outlook.com>
2019-10-25 14:49:27 +0800

    增加骁龙801的动态响应配置

---
+++ app/src/main/assets/powercfg/msm8974/powercfg-base-bigcore.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+
+echo 1 > /sys/devices/system/cpu/cpu0/online
+echo 1 > /sys/devices/system/cpu/cpu1/online
+echo 1 > /sys/devices/system/cpu/cpu2/online
+echo 1 > /sys/devices/system/cpu/cpu3/online
+
+stop thermal-engine
+
+echo 0 > /sys/module/msm_thermal/core_control/enabled
+echo 0 > /sys/module/msm_thermal/vdd_restriction/enabled
+echo N > /sys/module/msm_thermal/parameters/enabled
