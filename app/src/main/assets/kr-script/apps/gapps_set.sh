da8da70d686bd4d6e16a0ab7d9d0429d32ba3e67
kuanglinfei <helloklf@outlook.com>
2020-01-16 10:08:18 +0800

    Update gapps_set.sh

--- app/src/main/assets/kr-script/apps/gapps_set.sh
+++ app/src/main/assets/kr-script/apps/gapps_set.sh
@@ -3,9 +3,15 @@
 pm_cmd=""
 if [[ "$state" = "1" ]]; then
     pm_cmd="enable"
+    echo '√√  启用  Google服务套件'
 else
     pm_cmd="disable"
+    echo '××  停用  Google服务套件'
 fi
+
+echo ''
+echo ''
+
 
 pm $pm_cmd com.google.android.gsf 2> /dev/null
 pm $pm_cmd com.google.android.gsf.login 2> /dev/null
