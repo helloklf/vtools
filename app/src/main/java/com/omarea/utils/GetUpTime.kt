5a7fdb6f723e8c7149853161a472dfb16bb3ec8c
kuanglinfei <helloklf@outlook.com>
2020-01-17 14:43:04 +0800

    更新计算起床时间的函数

--- app/src/main/java/com/omarea/utils/GetUpTime.kt
+++ app/src/main/java/com/omarea/utils/GetUpTime.kt
@@ -1,14 +1,23 @@
 package com.omarea.utils
 
+import java.util.*
+
 // 计算还有多久起床
 // getUp = hours * 60 + minutes， 例如 6:30 表示为 6 * 60 + 30 = 390
 class GetUpTime(private val getUp: Int) {
+    // 现在时间
+    val currentTime: Int
+        get () {
+            val now = Calendar.getInstance()
+            return now.get(Calendar.HOUR_OF_DAY) * 60 + now.get(Calendar.MINUTE)
+        }
+
     // 距离下次起床还有多少分钟
     val minutes: Int
         get() {
-            val nowTimeValue = System.currentTimeMillis()
+            val nowTimeValue = currentTime
             // 距离起床的剩余时间（分钟）
-            var timeRemaining = 0L
+            var timeRemaining = 0
             // 和计算闹钟距离下一次还有多久响的逻辑有点像
             // 如果已经过了今天的起床时间，计算到明天的起床时间还有多久
             if (nowTimeValue > getUp) {
