3d56efe27c8f7444117d8615c54c0672bc8b7d03
kuanglinfei <helloklf@outlook.com>
2020-01-19 10:50:08 +0800

    更新模板

--- app/src/main/assets/powercfg/powercfg-template/conservative.sh
+++ app/src/main/assets/powercfg/powercfg-template/conservative.sh
@@ -6,19 +6,19 @@ if [[ "$action" = "init" ]] && [[ -f '/data/powercfg-base.sh' ]]; then
 	exit 0
 fi
 
-if [ "$action" = "powersave" ]; then
+if [[ "$action" = "powersave" ]]; then
     #powersave
 
 	exit 0
-elif [ "$action" = "balance" ]; then
+elif [[ "$action" = "balance" ]]; then
 	#balance
 
 	exit 0
-elif [ "$action" = "performance" ]; then
+elif [[ "$action" = "performance" ]]; then
 	#performance
 
 	exit 0
-elif [ "$action" = "fast" ]; then
+elif [[ "$action" = "fast" ]]; then
 	#fast
 	
 	exit 0
