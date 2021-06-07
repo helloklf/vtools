d7983a75cdb294fe336e3a091aa08edb3275b2c3
kuanglinfei <helloklf@outlook.com>
2021-06-07 16:30:29 +0800

    清理废弃的功能和代码

--- app/src/main/java/com/omarea/vtools/activities/ActivityFpsChart.kt
+++ app/src/main/java/com/omarea/vtools/activities/ActivityFpsChart.kt
@@ -47,8 +47,8 @@ class ActivityFpsChart : ActivityBase() {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
 
-
-        vtools_online.loadUrl("file:///android_asset/fps-chart/index.html")
+        // vtools_online.loadUrl("file:///android_asset/fps-chart/index.html")
+        vtools_online.loadUrl("https://vtools.oss-cn-beijing.aliyuncs.com/Scene-Online/fps-chart/index.html")
         val context = this@ActivityFpsChart
         val progressBarDialog = ProgressBarDialog(context)
 
