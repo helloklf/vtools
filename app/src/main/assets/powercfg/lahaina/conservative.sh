e9956244583de6ae5c02819fb09d224f8e4ed77f
kuanglinfei <helloklf@outlook.com>
2021-07-06 15:27:41 +0800

    更新至4.4.13 Release

--- app/src/main/assets/powercfg/lahaina/conservative.sh
+++ app/src/main/assets/powercfg/lahaina/conservative.sh
@@ -61,8 +61,8 @@ elif [[ "$action" = "balance" ]]; then
   cpuset '0-2' '0-3' '0-6' '0-7'
   stune_top_app 0 0
   # cpuctl top-app 0 1 0.1 max
-  cpuctl top-app 0 1 0.1 max
-  echo 75000000 > /sys/class/devfreq/1d84000.ufshc/max_freq
+  cpuctl top-app 0 1 0 max
+  echo 300000000 > /sys/class/devfreq/1d84000.ufshc/max_freq
 
 
 elif [[ "$action" = "performance" ]]; then
