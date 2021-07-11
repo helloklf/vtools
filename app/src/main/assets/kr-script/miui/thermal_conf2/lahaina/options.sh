050b0a6c2c7c8bcd11381b767b14de77c668a191
kaunglinfei <helloklf@outlook.com>
2021-07-11 22:51:21 +0800

    更新Mi11/11Pro/11Ultra/K40Pro的danger档温控配置
    主要是解决在最新的开发测试版系统上出现的跳电和温度不刷新问题

--- app/src/main/assets/kr-script/miui/thermal_conf2/lahaina/options.sh
+++ app/src/main/assets/kr-script/miui/thermal_conf2/lahaina/options.sh
@@ -6,7 +6,7 @@ echo "default|系统默认(default)
 cool_11|清爽酷凉(cool)
 pro_11|深度定制(pro)
 extreme_11|极致性能(extreme)
-danger|丧心病狂(danger)"
+danger_11|丧心病狂(danger)"
 
 # Mi 11 Pro、Ultra
 elif [[ $device == "M2102K1AC" ]] || [[ "$device" == 'M2102K1C' ]]; then
@@ -14,14 +14,14 @@ echo "default|系统默认(default)
 cool_11pro|清爽酷凉(cool)
 pro_11pro|深度定制(pro)
 extreme_11pro|极致性能(extreme)
-danger|丧心病狂(danger)"
+danger_11pro|丧心病狂(danger)"
 
 # K40 Pro
 elif [ $device == "M2012K11C" ]; then
 echo "default|系统默认(default)
 slight_k40pro|轻微调整(slight)
 pro_k40pro|深度定制(pro)
-danger|丧心病狂(danger)"
+danger_k40pro|丧心病狂(danger)"
 
 else
 echo "default|系统默认(default)
