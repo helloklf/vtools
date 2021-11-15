8c54798d9666fbf1f428512a77c943193dc3d6c4
kuanglinfei <helloklf@outlook.com>
2021-11-15 14:39:17 +0800

    更改亮屏状态判定逻辑

--- app/src/main/java/com/omarea/library/basic/ScreenState.kt
+++ app/src/main/java/com/omarea/library/basic/ScreenState.kt
@@ -7,22 +7,25 @@ import android.view.Display
 import android.view.WindowManager
 
 class ScreenState(private var context: Context) {
+    private val mKeyguardManager = context.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+    private val screenOffStatus = intArrayOf(
+            Display.STATE_OFF,
+            Display.STATE_DOZE,
+            Display.STATE_DOZE_SUSPEND,
+            Display.STATE_ON_SUSPEND
+    ).toTypedArray()
+
     fun isScreenLocked(): Boolean {
         val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
         val display = windowManager.defaultDisplay
-        if (display.state == Display.STATE_OFF) {
+        if (screenOffStatus.contains(display.state)) {
             return true
         }
 
-        val mKeyguardManager = context.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             mKeyguardManager.isDeviceLocked || mKeyguardManager.isKeyguardLocked
         } else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
-                mKeyguardManager.inKeyguardRestrictedInputMode() || mKeyguardManager.isDeviceLocked || mKeyguardManager.isKeyguardLocked
-            } else {
-                mKeyguardManager.inKeyguardRestrictedInputMode() || mKeyguardManager.isKeyguardLocked
-            }
+            mKeyguardManager.inKeyguardRestrictedInputMode() || mKeyguardManager.isDeviceLocked || mKeyguardManager.isKeyguardLocked
         }
     }
 
